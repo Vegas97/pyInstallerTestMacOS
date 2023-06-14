81cd4af19e1c493d11b228415b5f56c3f3bba8c3
Diego Palo <dp0997@gmail.com>
2023-06-14 23:43:27 +0200

    static data

--- main.py
+++ main.py
@@ -1,16 +1,82 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import sys
+import os
+from PySide6.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget
+from PySide6.QtGui import QPixmap, QImage, QPainter
+from PySide6.QtSvg import QSvgRenderer
+from PySide6.QtCore import Qt
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def resource_path0(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(
+        sys,
+        '_MEIPASS',
+        os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def resource_path(relative):
+    return os.path.join(
+        os.environ.get(
+            "_MEIPASS",
+            os.path.abspath(".")
+        ),
+        relative
+    )
+
+
+def resource_path2(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+def resource_path3(relative_path):
+    bundle_dir = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
+    return os.path.abspath(os.path.join(bundle_dir, relative_path))
+
+
+class ImageWindow(QWidget):
+    def __init__(self, image_path):
+        super().__init__()
+
+        # Load an SVG file using QSvgRenderer and render it into QPixmap
+        if image_path.lower().endswith('.svg'):
+            renderer = QSvgRenderer(image_path)
+            image = QImage(renderer.defaultSize(), QImage.Format_ARGB32)
+            image.fill(Qt.transparent)
+            painter = QPainter(image)
+            renderer.render(painter)
+            pixmap = QPixmap.fromImage(image)
+        else:
+            pixmap = QPixmap(image_path)
+
+        # Create a label and set the QPixmap as its content
+        label = QLabel(self)
+        label.setPixmap(pixmap)
+
+        # Create a layout and add the label to it
+        layout = QVBoxLayout(self)
+        layout.addWidget(label)
+
+        # Set layout
+        self.setLayout(layout)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    # Use resource_path to get the correct path for the image
+    image_path = resource_path3("test.png")  # Provide your image path here
+
+    print(image_path)
+
+    window = ImageWindow(image_path)
+    window.show()
+
+    sys.exit(app.exec())
---
+++ os_config/mac/build_mac.py
@@ -0,0 +1,109 @@
+import os
+import sys
+import shutil
+import subprocess
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def resource_path(relative_path):
+    bundle_dir = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
+    return os.path.abspath(os.path.join(bundle_dir, relative_path))
+
+
+appName = os.getenv('APP_NAME')  # money-manager-python
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+# Get the root directory
+root_directory = os.path.dirname(os.path.dirname(base_path))
+
+generatedDir = os.path.join(base_path, '_generated')
+# resourceDir = resource_path(os.path.join(root_directory, 'src', 'resources'))
+# systemTrayResDir = resource_path(os.path.join(resourceDir, 'images', 'system_tray'))
+
+print('---------------------------------------------')
+print('---------------------------------------------')
+print('---------------------------------------------')
+print(f'APP_NAME: {appName}')
+print(f'root_directory:   {root_directory}')
+print(f'base_path:        {base_path}')
+print(f'generatedDir:     {generatedDir}')
+# print(f'resourceDir:      {resourceDir}')
+# print(f'systemTrayResDir: {systemTrayResDir}')
+print('---------------------------------------------')
+print('---------------------------------------------')
+print('---------------------------------------------\n')
+
+# Clear: delete generatedDir directory
+if os.path.exists(generatedDir):
+    print('\nClearing generatedDir directory...')
+    shutil.rmtree(generatedDir)
+    print('Cleared generatedDir directory.\n')
+
+# Step 0: give some info
+subprocess.run([
+    'echo',
+    # f'{print(os.environ)}'
+    f'\nBuilding {appName}.app for Mac... (this may take a while; destination: {generatedDir})\n',
+])
+
+# Step 1: Create the executable
+subprocess.run([
+    'pyinstaller',
+    '--onefile',
+    "--windowed",
+    f"--distpath={generatedDir}/dist",
+    f"--workpath={generatedDir}/build",
+    f"--specpath={generatedDir}",
+    f"--add-data={root_directory}/test.png:Resources/images/system_tray",
+    f"--name={appName}",
+    f"{root_directory}/main.py"
+])
+
+# # Step 2: Create the .app bundle
+# subprocess.run(['mkdir', f'{appName}.app'])
+# subprocess.run(['mkdir', '-p', f'{appName}.app/Contents/MacOS'])
+# subprocess.run(['mv', f'dist/{appName}', f'{appName}.app/Contents/MacOS/'])
+#
+# # Optionally, create an Info.plist file here
+# subprocess.run([
+#     "/usr/libexec/PlistBuddy",
+#     "-c",
+#     "Add :CFBundleURLTypes array",
+#     f"dist/{appName}.app/Contents/Info.plist",
+# ])
+# subprocess.run([
+#     "/usr/libexec/PlistBuddy",
+#     "-c",
+#     "Add :CFBundleURLTypes:0 dict",
+#     f"dist/{appName}.app/Contents/Info.plist",
+# ])
+# subprocess.run([
+#     "/usr/libexec/PlistBuddy",
+#     "-c",
+#     "Add :CFBundleURLTypes:0:CFBundleURLSchemes array",
+#     f"dist/{appName}.app/Contents/Info.plist",
+# ])
+# subprocess.run([
+#     "/usr/libexec/PlistBuddy",
+#     "-c",
+#     f"Add :CFBundleURLTypes:0:CFBundleURLSchemes:0 string {appName}",
+#     f"dist/{appName}.app/Contents/Info.plist",
+# ])
+#
+# # Step 3: Create the DMG file
+# subprocess.run([
+#     '/path_to/create-dmg/create-dmg',
+#     '--volname', f'{appName}',
+#     '--volicon', f'os_config/mac/images/installer/installer-icon.png',
+#     '--background', 'os_config/mac/images/installer/background-installer.png',
+#     '--window-pos', '200', '120',
+#     '--window-size', '800', '400',
+#     '--icon-size', '100',
+#     '--icon', f'{appName}.app', '200', '190',
+#     '--hide-extension', f'{appName}.app',
+#     '--app-drop-link', '600', '185',
+#     f'{appName}.dmg',
+#     f'{appName}.app',
+# ])
